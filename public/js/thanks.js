a1945b7bef47c79176203beafe09c439a16bbd98
Setor <zilevus@vt.edu>
2017-11-17 10:01:29 -0500

    survey included plus feedback

--- public/js/thanks.js
+++ public/js/thanks.js
@@ -26,10 +26,6 @@ $(window).load(function() {
 
           var retVal;
 
-          var avgSC;
-          avgSC = (speedScore+compScore)/2;
-          avgSC = Math.round(avgSC);
-
           if (placScore >= 4 && taskScore >= 4 && avgSC >=4)
           {
             //excelent work
@@ -48,7 +44,7 @@ $(window).load(function() {
             $("#scoreContent").text("Nice try!");
           }
 
-          else if (placScore < 4)
+          else if (p < 4)
           {
             //nice Try, check placement.
             $("#scoreContent").text("Nice try! Check placement.");
@@ -59,10 +55,9 @@ $(window).load(function() {
 
           // Returns the success message on how the patient did and sets
           // the background color accordingly.
-          /*
-        //  var retVal = scoreToValues(avg);
-          //$("#scoreContent").text("Debug score:    " + retVal.response);
-          //$("#scoreContentDiv").css("background-color", retVal.hex);*/
+          var retVal = scoreToValues(avg);
+          $("#scoreContent").text("Debug score:    " + retVal.response);
+          //$("#scoreContentDiv").css("background-color", retVal.hex);
 
           //$("#prep").text(scoreresponse.toString());
           //$('#noticeContent').text(scoreresponse);
@@ -77,33 +72,7 @@ $(window).load(function() {
 
     	});
       // for the error runs we want to only display feedback for the incorrect run, not total
-
-
-        if (placScore >= 4 && taskScore >= 4 && avgSC >=4)
-        {
-          //excelent work
-          $("#scoreContent").text("Excellent work!");
-        }
-
-        else if (placScore >= 4 && taskScore >= 4 && avgSC < 4)
-        {
-          //good
-          $("#scoreContent").text("Good job!");
-        }
-
-        else if (taskScore < 4 )
-        {
-          //nice try!
-          $("#scoreContent").text("Nice try!");
-        }
-
-        else if (p < 4)
-        {
-          //nice Try, check placement.
-          $("#scoreContent").text("Nice try! Check placement.");
-        }
-
-    /*  function scoreToValues(score){
+      function scoreToValues(score){
     		console.log("scoreToValues");
     		var hex="";
     		var response="";
@@ -130,7 +99,7 @@ $(window).load(function() {
     			hex: hex,
     			response: response
     		};
-	}*/
+	}
 
     /*  document.getElementById("tg-i4").innerHTML = scoreresponse.score4;
       document.getElementById("tg-i5").innerHTML = scoreresponse.score5;
