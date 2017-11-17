e39f216234b9e3f5f891cbaa02f5c333ebc5e7d4
Setor <zilevus@vt.edu>
2017-11-16 20:12:28 -0500

    start recording added

--- public/js/preactivity.js
+++ public/js/preactivity.js
@@ -2,6 +2,6 @@ var socket = io();
 var loopNum = sessionStorage.getItem("loopNum");
 var exerciseNum = sessionStorage.getItem("exerciseNum");
 function notify() {
-    var startTask="{\"type\" : \"recordTask\", \"task\" : "+exerciseNum.toString()+"}";
+    var startTask="{\"type\" : \"startTask\", \"task\" : "+exerciseNum.toString()+"}";
     socket.emit("json", startTask);
 }
--- public/js/sitBack.js
+++ public/js/sitBack.js
@@ -1,23 +1,11 @@
-//var socket = io();
-$(window).load(function() {
-    /*var socket = io();
-    if (socket == null) {
-        socket = io();
-    }*/
-	// session storage?
-	// sets the initial exercise
+var socket = io();
+var loopNum = sessionStorage.getItem("loopNum");
+var exerciseNum = sessionStorage.getItem("exerciseNum");
+function notify() {
+    var startTask="{\"type\" : \"startRecording\", \"task\" : "+exerciseNum.toString()+"}";
+    socket.emit("json", startTask);
+}
 
-	var taskSetupReq="{\"type\" : \"taskSetup2\",\"task\" : ";
-	socket.emit("json", taskSetupReq +"10"+"}");
-	var loopNum = sessionStorage.getItem("loopNum");
-	var exerciseNum = sessionStorage.getItem("exerciseNum");
-	// Because the first screen handles the getting of these variables in
-	// session storage we can reuse them anywhere throughout the application
-    var dat = new Date();
-    var fulltime = dat.toJSON();
-    //var retMsg = "{\"page\" : \"tasksetup\", \"access time\" : "+"\""+fulltime+"\""+ ", \"iteration\" : "+"\""+loopNum.toString()+"\""+"\"exercise\" : "+"\""+exerciseNum.toString()+"\""+"}";
-    //    io().emit("json", retMsg);
-    // sets the exercise picture equal to necessary number
 
 
 
