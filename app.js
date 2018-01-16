ca009a2df5567af28ebd2dd45fae3486a8972275
Setor <zilevus@vt.edu>
2018-01-16 15:49:55 -0500

    updated changes

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const logger = new Console(process.stdout, process.stderr);
   console.log('Connected');
 });*/
 var HOST='localhost'; //Change this back to localhost
+
 //var HOST='128.173.21.172'; //Change this to whomever is hosting
 var PORT=8888;
 client.connect(PORT,HOST,function(){
