1aff8cded0ee93a825ef25b0715f97844a4a3fc2
Setor <zilevus@vt.edu>
2018-01-26 10:58:43 -0500

    left/moderate changes:

--- public/js/conclusion.js
+++ public/js/conclusion.js
@@ -8,6 +8,74 @@ $(window).load(function() {
 	// sets the initial exercise
 	var loopNum = sessionStorage.getItem("loopNum");
 	var exerciseNum = sessionStorage.getItem("exerciseNum");
+	var socket = io();
+		console.log("HERE first");
+		var taskLoad=false;
+		var socket=io();
+		var taskname=document.getElementById("taskName");
+		var endTask="{\"type\" : \"endTask\"}";
+		socket.emit("json", endTask);
+		socket.on('message',function(data){
+			console.log("HERE");
+			var html="";
+			if(data.message!=null){
+				var scoreresponse=JSON.parse(data.message);
+				var s1 = scoreresponse.score1; //hand
+				var s2 = scoreresponse.score2; //repeat
+
+
+				var right = Math.round(s1);
+				var repeat = Math.round(s2);
+
+				if (right == 1)
+				{
+						$("#scoreContent").text("right!");
+				}
+
+				else if (repeat == 0 )
+				{
+					$("#scoreContent").text("repeat!");
+				}
+
+			var retVal = scoreToValues(placScore, taskScore, avgSC);
+			$("#scoreContent").text(retVal.response);
+			console.log(scoreresponse);
+
+			function scoreToValues(placScore, taskScore, avgSC){
+				console.log("scoreToValues");
+				var hex="";
+				var response="";
+			if (right == 1 ){
+					//excelent work
+					response = "Excellent work!"
+					}
+			else if (right == 0)
+					{
+						response = "Good job!"
+					}
+			else if (repeat == 0 )
+					{
+						response = "Nice try!"
+					}
+			else if (repeat == 1)
+					{
+						//nice Try, check placement.
+					response = "Nice tryyyy!";
+					}
+			else {
+					response="No score available";
+					hex="#ffffff";
+				}
+				return {
+					hex: hex,
+					response: response
+				};
+	}
+
+				//if its rightHanded send 1
+				// if left handed send 0
+				//if repeatFlag send 1
+				// if notRepeatFlag send 0
 	// Because the first screen handles the getting of these variables in
 	// session storage we can reuse them anywhere throughout the application
     var dat = new Date();
